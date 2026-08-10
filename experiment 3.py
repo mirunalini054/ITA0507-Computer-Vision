7b34906649daaf20cc8926d7c8feff8092ea979e
mirunalini054 <192521054.simats@saveetha.com>
2026-08-10 23:26:23 +0530

    Add files via upload

---
+++ experiment 3.py
@@ -0,0 +1,9 @@
+import cv2
+image = cv2.imread("C:/Users/Admin/Downloads/download (1).jfif")
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray_image, 100, 200) # 100 and 200 are threshold values
+cv2.imshow("Original Image", image)
+cv2.imshow("Edge Detected Image", edges)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
---
+++ experiment 5.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+def analyze_histogram(image_path):
+    image = cv2.imread(image_path)
+    if image is None:
+        print("Error: Could not load image.")
+        return
+    color_channels = ('b', 'g', 'r')
+    plt.figure(figsize=(10, 5))
+    for i, color in enumerate(color_channels):
+        histogram = cv2.calcHist([image], [i], None, [256], [0, 256])
+        plt.plot(histogram, color=color, label=f"{color.upper()} Channel")
+    plt.xlim([0, 256])
+    plt.title("Color Histogram Analysis")
+    plt.xlabel("Pixel Intensity")
+    plt.ylabel("Frequency")
+    plt.legend()
+    plt.show()
+analyze_histogram(r"C:/Users/Admin/Downloads/download (1).jfif")
